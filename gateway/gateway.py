6d9c8811606ec4bc369924593275523d778c3d05
OmegaMaker <luka1837@gmail.com>
2025-12-09 13:18:00 +0100

    Added logging to gateway

--- gateway/gateway.py
+++ gateway/gateway.py
@@ -1,7 +1,15 @@
 from flask import Flask, request, render_template_string, jsonify
 import requests
+import logging
 
 app = Flask(__name__)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
+
+logger = logging.getLogger(__name__)
 
 last_received = "Nothing received yet."
 
@@ -26,6 +34,10 @@ def view():
 def receive():
     global last_received
     last_received = request.get_data(as_text=True)
+
+    ip = request.remote_addr
+    logger.info("POST /test from %s | Data: %s", ip, last_received)
+
     return "OK", 200
 
 @app.route("/card-scan", methods=["POST"])
@@ -34,17 +46,25 @@ def cardScan():
         payload = request.get_json()
         uid = payload.get("uid")
 
+        ip = request.remote_addr
+        logger.info("POST /card-scan from %s | Data: %s", ip, payload)
+
+        if not payload or "uid" not in payload:
+            logger.info("Invalid JSON data")
+            return jsonify({"error": "Invalid JSON payload"}), 400
+
         response = requests.post("http://receiver-service:8000/authorize", json=payload, timeout=2)
         data = response.json()
 
-        access = data.get("access")
+        logger.info("POST /authorize to http://receiver-service:8000 | Data: %s | Response: %s", payload, data)
 
         return jsonify({
-            "access": "granted" if access else "denied",
+            "access": data.get("access"),
             "uid": uid
         }), 200
 
     except Exception as e:
+        logger.info("Error with card scan")
         print("CARD SCAN ERROR:", repr(e))
         return jsonify({
             "error": str(e)
